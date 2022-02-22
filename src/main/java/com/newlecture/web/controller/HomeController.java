eb1ba589df3857d6a5f59e57f7012a2e838c8a1f
yamto <yamto@118.34.83.85>
2022-02-23 04:10:14 +0900

    컨트롤러 정리하기

---
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.newlecture.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+	
+	@RequestMapping("index")
+	public String index() {
+		
+		return "root.index";
+	}
+	
+//	@Override
+//	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+//		ModelAndView mv = new ModelAndView("root.index");
+//		mv.addObject("data", "Hello Spring MVC~");
+//		//mv.setViewName("/WEB-INF/view/index.jsp");
+//
+//		return mv;
+//	}
+	
+}
