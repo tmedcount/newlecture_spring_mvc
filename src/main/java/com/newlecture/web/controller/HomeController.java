a862566000c7ccb7116d7a0991b79faf9d6100ff
yamto <yamto@118.34.83.85>
2022-02-23 11:51:44 +0900

    문서 출력방법 4가지 @ResponseBody

--- src/main/java/com/newlecture/web/controller/HomeController.java
+++ src/main/java/com/newlecture/web/controller/HomeController.java
@@ -1,18 +1,23 @@
 package com.newlecture.web.controller;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
 	
 	@RequestMapping("index")
+	@ResponseBody //= 서블릿 객체를 얻어서 문자열 출력하기(HttpServletResponse)
 	public String index() {
 		
-		System.out.println("index 요청이 있었습니다.");
-		return "root.index";
-
+		return "Hello Index";
 	}
 	
 //	@Override
